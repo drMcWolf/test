b995d84e9b66ab4fa7ac8ff6b7e4ba0db69193ba
Иван Макаров <ivan.makarov@academmedia.com>
2016-03-19 14:59:14 +0600

    add advance new obj

---
+++ qweas/qweas/advNewObj.swift
@@ -0,0 +1,12 @@
+//
+//  advNewObj.swift
+//  qweas
+//
+//  Created by Ivan Makarov on 19.03.16.
+//  Copyright © 2016 Ivan Makarov. All rights reserved.
+//
+
+import Foundation
+class advNewObj {
+    var test = "test"
+}
