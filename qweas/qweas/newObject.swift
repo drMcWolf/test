eb86b9b5dcf4dc9070bd625d807f4127b9fbac16
Иван Макаров <ivan.makarov@academmedia.com>
2016-03-19 14:35:15 +0600

    newobj

---
+++ qweas/qweas/newObject.swift
@@ -0,0 +1,12 @@
+//
+//  newObject.swift
+//  qweas
+//
+//  Created by Ivan Makarov on 19.03.16.
+//  Copyright © 2016 Ivan Makarov. All rights reserved.
+//
+
+import Foundation
+class newObject {
+    var qq = 0
+}
