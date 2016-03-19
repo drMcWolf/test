19d256f27ffd4485c86c5771df69641a3e33e5a9
Иван Макаров <ivan.makarov@academmedia.com>
2016-03-19 13:45:14 +0600

    sinfleton

---
+++ qweas/qweas/DataManager.swift
@@ -0,0 +1,12 @@
+//
+//  DataManager.swift
+//  qweas
+//
+//  Created by Ivan Makarov on 19.03.16.
+//  Copyright © 2016 Ivan Makarov. All rights reserved.
+//
+
+import Foundation
+class controller {
+    static let qq = controller()
+}
